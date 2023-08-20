35f3e3c35e8f7a5f16655b58c8f4a44a1b683354
huy-lv <huylv177@gmail.com>
2023-08-20 16:29:36 +0700

    Add project files.

---
+++ WindowsService1/ProjectInstaller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+
+namespace WindowsService1
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : ServiceInstaller
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+            this.AfterInstall += serviceInstaller1_AfterInstall;
+            //serviceInstaller1.Committed += OnCommitted;
+        }
+
+        //public void OnCommitted(object sender, InstallEventArgs e)
+        //{
+        //    using (ServiceController sc = new ServiceController(serviceInstaller1.ServiceName))
+        //    {
+        //        sc.Start();
+        //    }
+        //}
+
+        private void serviceInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+            using (ServiceController sc = new ServiceController(serviceInstaller1.ServiceName))
+            {
+                sc.Start();
+            }
+        }
+        //protected override void OnCommitted(System.Collections.IDictionary savedState)
+        //{
+        //    ServiceController sc = new ServiceController("Service1");
+        //    sc.Start();
+        //}
+    }
+}
